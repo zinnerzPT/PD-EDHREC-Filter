e52a4da7f5eb682eead43baa8fafcf7e9f08abea
Francisco Domingues <francisco.domingues@redcatpig.com>
2023-12-18 15:29:52 +0000

    Uploaded PD EDHREC Filter userscript

---
+++ PD EDHREC Filter.user.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name         PD EDHREC Filter
+// @namespace    zinnerzPT
+// @version      0.9
+// @description  Hides non-legal Penny Dreadful cards in EDHREC
+// @author       zinnerzPT
+// @match        https://edhrec.com/*
+// @grant        GM_xmlhttpRequest
+// @homepageURL  https://github.com/zinnerzPT/PD-EDHREC-Filter
+// @supportURL   https://github.com/zinnerzPT/PD-EDHREC-Filter/issues
+// @downloadURL  https://github.com/zinnerzPT/PD-EDHREC-Filter/raw/main/PD%20EDHREC%20Filter.user.js
+// @updateURL    https://github.com/zinnerzPT/PD-EDHREC-Filter/raw/main/PD%20EDHREC%20Filter.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let legalCardNames;
+
+    // Hides cards based on names and the current toggle state
+    function hideCardsByNames(hideCards) {
+        const cardWrappers = document.querySelectorAll('[class*="CardView_cardWrapper"]');
+        cardWrappers.forEach(cardWrapper => {
+            let nameWrapper = cardWrapper.querySelector('[class*="Card_name"]');
+            if (!nameWrapper) {
+                nameWrapper = cardWrapper.querySelector('[class*="Card_nameUnderCard"]'); // if Names Under Cards option is ticked
+            }
+            if (nameWrapper) {
+                const cardName = nameWrapper.textContent.trim();
+                const isLegal = legalCardNames.includes(cardName);
+                const shouldHide = hideCards ? !isLegal : false;
+                cardWrapper.style.display = shouldHide ? 'none' : 'block';
+            }
+        });
+    }
+
+    // Creates and appends the toggle button to the navbar
+    function createToggleButton(hideCards) {
+        const navbar = document.querySelector('.Navbar_buttonContainer__A2QR6.navbar-nav');
+        const toggleContainer = document.createElement('div');
+        toggleContainer.className = 'Navbar_buttonContainer__A2QR6 toggle-container';
+        const toggleButton = document.createElement('div');
+        toggleButton.className = 'toggle-button';
+        toggleButton.innerHTML = '<span class="toggle-label">PD Filter </span><label class="switch"><input type="checkbox" id="toggleSwitch"><span class="slider"></span></label>';
+        const toggleSwitch = toggleButton.querySelector('#toggleSwitch');
+        toggleSwitch.checked = hideCards;
+        toggleButton.addEventListener('click', function() {
+            hideCards = !hideCards;
+            localStorage.setItem('hideCardsToggle', hideCards);
+            toggleSwitch.checked = hideCards;
+            hideCardsByNames(hideCards);
+        });
+        toggleContainer.appendChild(toggleButton);
+        navbar.appendChild(toggleContainer);
+    }
+
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: 'http://pdmtgo.com/legal_cards.txt',
+        onload: function(response) {
+            if (response.status === 200) {
+                legalCardNames = response.responseText.split('\n').map(name => name.trim());
+                let hideCards = localStorage.getItem('hideCardsToggle') === 'true';
+                hideCardsByNames(hideCards);
+                createToggleButton(hideCards);
+                window.addEventListener('scroll', function() {
+                    if (localStorage.getItem('hideCardsToggle') === 'true') {
+                        hideCardsByNames(true);
+                    }
+                });
+            } else {
+                console.error('Failed to fetch legal card names');
+            }
+        }
+    });
+})();
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PD-EDHREC-Filter
-Hides non-legal Penny Dreadful cards in EDHREC
+This is a simple userscript that adds a toggle on the top right corner of the window in EDHREC to automatically hide cards that aren't legal in Penny Dreadful.
+
+# Instructions
+1. Install [Tampermonkey](https://tampermonkey.net/)
+2. Install [PD EDHREC Filter](https://github.com/zinnerzPT/PD-EDHREC-Filter/raw/main/PD%20EDHREC%20Filter.user.js)
+3. Enjoy!
+
+# Change Log
+ * *Version 0.9.0*
+     * Initial commit
